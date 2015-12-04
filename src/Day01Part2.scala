7fa53afe1f2507e1566214e78a97010094f74a4b
Jesse Rohland <jesse.rohland@gmail.com>
2015-12-03 21:19:53 -0500

    Use folding to track current floor

--- src/Day01Part2.scala
+++ src/Day01Part2.scala
@@ -4,20 +4,22 @@
 object Day01Part2 {
 
   def main(args: Array[String]): Unit = {
-    var floor = 0
-    var curStep = 1
-    var firstBasement = -1
-    val input = scala.io.Source.fromFile("input/day01-input.txt").getLines().next()
-    input.foreach(direction => {
-      direction match {
-        case '(' => floor += 1
-        case ')' => floor -= 1
-      }
-      if (floor == -1 && firstBasement == -1)
-        firstBasement = curStep
-      curStep += 1
+    val input = scala.io.Source.fromFile("input/day01-input.txt").getLines().next().map({
+      case '(' => 1
+      case ')' => -1
     })
-    println(s"Hit basement on step $firstBasement")
+
+    // Fold a tuple of current floor and floor count
+    input.foldLeft((0, 0))((x, y) => {
+      if (x._1 + y == -1) {
+        println(s"Hit basement on step ${x._2 + 1}")
+        return
+      }
+      (x._1 + y, x._2 + 1)
+    })
+
+    println("Never hit basement")
+
   }
 
 }
