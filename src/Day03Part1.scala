2d68f1f0f50a4aaff93387fa861e6b453b0b1d2c
Jesse Rohland <jesse.rohland@gmail.com>
2015-12-03 21:30:14 -0500

    Use tuples and scanLeft to track location

--- src/Day03Part1.scala
+++ src/Day03Part1.scala
@@ -4,18 +4,17 @@
 object Day03Part1 {
 
   def main(args: Array[String]): Unit = {
-    var xPos = 0
-    var yPos = 0
-    val input = scala.io.Source.fromFile("input/day03-input.txt").getLines().next()
-    val delivered = (Array((0,0)) ++ input.map(direction => {
-      direction match {
-        case '^' => yPos += 1
-        case 'v' => yPos -= 1
-        case '>' => xPos += 1
-        case '<' => xPos -= 1
-      }
-      (xPos, yPos)
-    })).toSet
+    val input = scala.io.Source.fromFile("input/day03-input.txt").getLines().next().map({
+      case '^' => (0, 1)
+      case 'v' => (0, -1)
+      case '>' => (1, 0)
+      case '<' => (-1, 0)
+    })
+
+    val delivered = input.scanLeft((0, 0))((x, y) => {
+      (x._1 + y._1, x._2 + y._2)
+    }).toSet
+
     println(s"Unique places delivered to: ${delivered.size}")
   }
 
