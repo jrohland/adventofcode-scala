a3051d02e26d503ee828bd929cc2648c2506b0f5
Jesse Rohland <jesse.rohland@gmail.com>
2015-12-03 21:39:53 -0500

    Use tuples and scanLeft to track location

--- src/Day03Part2.scala
+++ src/Day03Part2.scala
@@ -4,25 +4,25 @@
 object Day03Part2 {
 
   def main(args: Array[String]): Unit = {
-    var xPos1 = 0
-    var yPos1 = 0
-    var xPos2 = 0
-    var yPos2 = 0
-    var isSantaMove = false
-    val input = scala.io.Source.fromFile("input/day03-input.txt").getLines().next()
-    val delivered = (Array((0,0)) ++ input.map(direction => {
-      isSantaMove = !isSantaMove
-      direction match {
-        case '^' => if (isSantaMove) yPos1 += 1 else yPos2 += 1
-        case 'v' => if (isSantaMove) yPos1 -= 1 else yPos2 -= 1
-        case '>' => if (isSantaMove) xPos1 += 1 else xPos2 += 1
-        case '<' => if (isSantaMove) xPos1 -= 1 else xPos2 -= 1
-      }
-      if (isSantaMove)
-        (xPos1, yPos1)
+    val input = scala.io.Source.fromFile("input/day03-input.txt").getLines().next().map({
+      case '^' => (0, 1)
+      case 'v' => (0, -1)
+      case '>' => (1, 0)
+      case '<' => (-1, 0)
+    })
+
+    // Scan with a tuple tracking two locations and a bool for which to update
+    val delivered = input.scanLeft((true, 0, 0, 0, 0))((x, y) => {
+      if (x._1)
+        (false, x._2 + y._1, x._3 + y._2, x._4, x._5)
       else
-        (xPos2, yPos2)
-    })).toSet
+        (true, x._2, x._3, x._4 + y._1, x._5 + y._2)
+    }).flatMap(pos => {
+      Array(
+        (pos._2, pos._3),
+        (pos._4, pos._5)
+      )
+    }).toSet
     println(s"Unique places delivered to: ${delivered.size}")
   }
 
