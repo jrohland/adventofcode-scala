49fb4b34f325fa1c22ee2a2b5372c44ca0d8daf7
Jesse Rohland <jesse.rohland@gmail.com>
2015-12-10 13:21:29 -0500

    Use char lists instead of string concats

--- src/Day10Part1.scala
+++ src/Day10Part1.scala
@@ -4,7 +4,7 @@
 object Day10Part1 {
 
   def main(args: Array[String]): Unit = {
-    val input = scala.io.Source.fromFile("input/day10-input.txt").getLines().next()
+    val input = scala.io.Source.fromFile("input/day10-input.txt").getLines().next().toCharArray.toList
     val encoded = (1 to 40).foldLeft(input)((x, y) => {
       println(s"Iteration $y")
       runLengthEncode(x)
@@ -12,15 +12,15 @@ object Day10Part1 {
     println(encoded.length)
   }
 
-  def runLengthEncode(str: String): String = {
+  def runLengthEncode(str: List[Char]): List[Char] = {
     str.foldLeft(List[(Char, Int)]())((x, y) => {
       x.headOption match {
         case Some(char) if char._1 == y => (y, char._2 + 1) :: x.tail
         case _ => (y, 1) :: x
       }
-    }).reverse.map(char => {
-      s"${char._2}${char._1}"
-    }).mkString
+    }).reverse.flatMap(char => {
+      char._2.toString.toCharArray :+ char._1
+    })
   }
 
 }
