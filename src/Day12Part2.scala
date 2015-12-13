b851a405a92c86f9a2df1784eb3e06070d6f963b
Jesse Rohland <jesse.rohland@gmail.com>
2015-12-12 20:03:29 -0500

    Clean up part 2

--- src/Day12Part2.scala
+++ src/Day12Part2.scala
@@ -9,19 +9,16 @@ object Day12Part2 {
     val input = scala.io.Source.fromFile("input/day12-input.txt").getLines().next()
     JSON.globalNumberParser = {in => try { in.toInt } catch { case e: Exception => in.toDouble}}
     val parsed = JSON.parseFull(input).get
-    val sum = findNumerics(parsed)._1
+    val sum = findNumerics(parsed)
     println(s"Total sum: $sum")
   }
 
-  def findNumerics(block: Any): (Int, Boolean) = {
+  def findNumerics(block: Any): Int = {
     block match {
-      case l: List[_] => (l.map(findNumerics).map(_._1).sum, false)
-      case m: Map[_, _] =>
-        val values = m.values.map(findNumerics).toList
-        if (values.exists(_._2)) (0, false) else (values.map(_._1).sum, false)
-      case i: Int => (i, false)
-      case "red" => (0, true)
-      case _ => (0, false)
+      case l: List[_] => l.map(findNumerics).sum
+      case m: Map[_, _] => if (m.values.toList.contains("red")) 0 else m.values.map(findNumerics).sum
+      case i: Int => i
+      case _ => 0
     }
   }
 
