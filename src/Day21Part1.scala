3445e1de620296d2e9ed5c45999539bd0221b8ec
Jesse Rohland <jesse.rohland@gmail.com>
2015-12-21 18:37:04 -0500

    Day 21

---
+++ src/Day21Part1.scala
@@ -0,0 +1,78 @@
+/**
+  * Created by jesse on 12/21/15.
+  */
+object Day21Part1 {
+
+  val store = Map(
+    "weapons" -> List(
+      new StoreItem("Dagger", 8, 4, 0),
+      new StoreItem("Shortsword", 10, 5, 0),
+      new StoreItem("Warhammer", 25, 6, 0),
+      new StoreItem("Longsword", 40, 7, 0),
+      new StoreItem("Greataxe", 74, 8, 0)
+    ),
+    "armor" -> List(
+      new StoreItem("Leather", 13, 0, 1),
+      new StoreItem("Chainmail", 31, 0, 2),
+      new StoreItem("Splintmail", 53, 0, 3),
+      new StoreItem("Bandedmail", 75, 0, 4),
+      new StoreItem("Platemail", 102, 0, 5)
+    ),
+    "rings" -> List(
+      new StoreItem("Damage +1", 25, 1, 0),
+      new StoreItem("Damage +2", 50, 2, 0),
+      new StoreItem("Damage +3", 100, 3, 0),
+      new StoreItem("Defense +1", 20, 0, 1),
+      new StoreItem("Defense +2", 40, 0, 2),
+      new StoreItem("Defense +3", 80, 0, 3)
+    )
+  )
+
+  def main(args: Array[String]): Unit = {
+    val input = scala.io.Source.fromFile("input/day21-input.txt").getLines()
+
+    val Array(bossHP, bossDamage, bossArmor) = input.map(line => {
+      line.split(": ").last.toInt
+    }).toArray
+
+    val weaponArmorCombos = for (weapon <- store("weapons"); armor <- store("armor")) yield weapon + armor
+    val ringCombos = store("rings").combinations(2).map(combo => {
+      combo.head + combo.last
+    })
+    val allCombos = weaponArmorCombos ++
+      (for (combo <- weaponArmorCombos; ring <- store("rings")) yield combo + ring) ++
+      (for (combo <- weaponArmorCombos; ring <- ringCombos) yield combo + ring)
+
+
+    val winningCombos = allCombos.filter(combo => {
+      fightRound(bossHP, 100, bossDamage, bossArmor, combo)
+    }).sortBy(_.cost)
+
+    winningCombos.foreach(println)
+  }
+
+  def fightRound(bossHP: Int, yourHP: Int, bossDamage: Int, bossArmor: Int, items: StoreItem): Boolean = {
+    val dmgToBoss = Math.max(items.damage - bossArmor, 0)
+    val dmgToYou = Math.max(bossDamage - items.armor, 0)
+
+    if (bossHP - dmgToBoss <= 0) {
+      true
+    } else if (yourHP - dmgToYou <= 0) {
+      false
+    } else {
+      fightRound(bossHP - dmgToBoss, yourHP - dmgToYou, bossDamage, bossArmor, items)
+    }
+  }
+
+  case class StoreItem(name: String, cost: Int, damage: Int, armor: Int) {
+    def +(that: StoreItem): StoreItem = {
+      StoreItem(
+        name = s"${this.name} & ${that.name}",
+        cost = this.cost + that.cost,
+        damage = this.damage + that.damage,
+        armor = this.armor + that.armor
+      )
+    }
+  }
+
+}
