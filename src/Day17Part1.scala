33b066e1b20fe0c6e9794cfc2d46bfcd97c591d2
Jesse Rohland <jesse.rohland@gmail.com>
2015-12-25 12:01:46 -0500

    Use zipWithIndex

--- src/Day17Part1.scala
+++ src/Day17Part1.scala
@@ -4,12 +4,10 @@
 object Day17Part1 {
 
   def main(args: Array[String]): Unit = {
-    val input = scala.io.Source.fromFile("input/day17-input.txt").getLines().toList
-    val containers = input.indices.map(i => {
-      i -> input(i).toInt
-    })
+    val input = scala.io.Source.fromFile("input/day17-input.txt").getLines().map(_.toInt).toList
+    val containers = input.zipWithIndex
     val combos = (1 to containers.size).flatMap(i => {
-      containers.combinations(i).filter(_.map(_._2).sum == 150)
+      containers.combinations(i).filter(_.map(_._1).sum == 150)
     })
     println(combos.size)
   }
--- src/Day17Part2.scala
+++ src/Day17Part2.scala
@@ -4,12 +4,10 @@
 object Day17Part2 {
 
   def main(args: Array[String]): Unit = {
-    val input = scala.io.Source.fromFile("input/day17-input.txt").getLines().toList
-    val containers = input.indices.map(i => {
-      i -> input(i).toInt
-    })
+    val input = scala.io.Source.fromFile("input/day17-input.txt").getLines().map(_.toInt).toList
+    val containers = input.zipWithIndex
     val combos = (1 to containers.size).map(i => {
-      i -> containers.combinations(i).filter(_.map(_._2).sum == 150)
+      i -> containers.combinations(i).filter(_.map(_._1).sum == 150)
     }).toMap.filter(_._2.nonEmpty)
     println(combos(combos.keys.toList.sortBy(identity).head).size)
   }
